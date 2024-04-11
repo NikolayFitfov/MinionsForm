c1790bd7ad11f01cb779d0b4b3bd8fbfca3ab83b
Nikolay <Nikolay.fitfov@gmail.com>
2024-04-11 08:34:01 +0300

    MinionsForm

---
+++ MinionsForm/ComboItem.cs
@@ -0,0 +1,11 @@
+﻿using Mysqlx.Connection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MinionsForm
+{
+    public class ComboItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+    }
+}
